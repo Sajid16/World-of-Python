0041fdcb42ec4d616eb664048d47098d6594798c
Sajid16 <sajid.mahboob16@gmail.com>
2019-08-06 12:51:20 +0600

    updated code from workplace

--- K_nearest_neighbor/Include/diabetes_test.py
+++ K_nearest_neighbor/Include/diabetes_test.py
@@ -1,4 +1,4 @@
-#KNN - predict whether a person have diabetes or not
+# KNN - predict whether a person have diabetes or not
 
 # importing important libraries
 import numpy as np
@@ -6,6 +6,7 @@ import pandas as pd
 import math
 
 from sklearn.model_selection import train_test_split
+from sklearn import preprocessing
 from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import confusion_matrix
@@ -26,33 +27,55 @@ for column in zero_not_accepted:
     #     print(i+2)
     dataset[column] = dataset[column].replace(0, np.NaN)
     mean = int(dataset[column].mean(skipna=True))
-    print(mean)
+    # print(mean)
     dataset[column] = dataset[column].replace(np.NaN, mean)
-print(dataset['Glucose'])
+# print(dataset['Glucose'])
 
-#splitting dataset
+# splitting dataset
 X = dataset.iloc[:, 0:8]
 y = dataset.iloc[:, 8]
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0, test_size=0.2)
 
+# print('before\n')
 # print(X_train)
 # print(X_test)
 # print(y_train)
 # print(y_test)
 
-#feature scaling
+# feature scaling
 sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.fit_transform(X_test)
 
-#define the model: init K-NN
-classifier = KNeighborsClassifier(n_neighbors=11, p=2, metric='euclidean')
+# print(X_train)
+# print(X_test)
 
-print(math.sqrt(len(y)))
 
-#fit model
+neighbor = int(math.sqrt(len(y_train)))
+if neighbor % 2 == 0:
+    neighbor = neighbor - 1
+#     print(neighbor)
+# print(neighbor)
+
+
+# define the model: init K-NN
+classifier = KNeighborsClassifier(n_neighbors=neighbor, p=2, metric='euclidean')
+
+# fit model
 classifier.fit(X_train, y_train)
+print('training score is:\n')
+print(classifier.score(X_train, y_train))
 
-#predict the test set result
+# predict the test set result
 y_pred = classifier.predict(X_test)
 print(y_pred)
+
+# testing the model result
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+
+f1score = f1_score(y_test, y_pred)
+print(f1score)
+
+accuracy = accuracy_score(y_pred, y_test)
+print(accuracy)
