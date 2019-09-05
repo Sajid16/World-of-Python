bad3e0d8527026831649c1691a4c292a364d156c
Sajid16 <sajid.mahboob16@gmail.com>
2019-09-05 11:21:53 +0600

    5th september from workplace

--- Basic_python_Practice/training_test_2.py
+++ Basic_python_Practice/training_test_2.py
@@ -4,7 +4,7 @@ number_of_player = input("enter the number of player for this game: ")
 number_of_player = int(number_of_player)
 
 if (number_of_player > 1) and (number_of_player <= 4):
-    final_point1, final_point2, final_point3, winner1, winner2, winner3 = 0, 0, 0, 0, 0, 0
+    final_point1, final_point2, final_point3, final_point4, winner1, winner2, winner3 = 0, 0, 0, 0, 0, 0, 0
     final_point, point2, point1, point3, point4 = 0, 0, 0, 0, 0
     p1, p2, p3, p4, d = 0, 0, 0, 0, 0
     if number_of_player == 2:
@@ -141,5 +141,40 @@ if (number_of_player > 1) and (number_of_player <= 4):
             print(player3_name, ' wins: ', p3, ' rounds')
             print('All draws: ', d, ' rounds')
             print(player3_name, " is the winner. Congratulations!")
+    # for 4 players
+
+    else:
+        player1_name = input("enter the first player name: ")
+        player2_name = input("enter the second player name ")
+        player3_name = input("enter the third player name ")
+        player4_name = input("enter the fourth player name ")
+        player1_name = player1_name.upper()
+        player2_name = player2_name.upper()
+        player3_name = player3_name.upper()
+        player4_name = player4_name.upper()
+
+        while final_point < 100:
+            point1 = random.randint(1, 7)
+            point2 = random.randint(1, 7)
+            point3 = random.randint(1, 7)
+            point4 = random.randint(1, 7)
+
+            final_point1 = final_point1 + point1
+            if final_point1 >= 100:
+                final_point = final_point1
+                print(player1_name, " is the winner. Congratulations!")
+            final_point2 = final_point2 + point2
+            if final_point2 >= 100:
+                final_point = final_point2
+                print(player2_name, " is the winner. Congratulations!")
+            final_point3 = final_point3 + point3
+            if final_point3 >= 100:
+                final_point = final_point3
+                print(player3_name, " is the winner. Congratulations!")
+            final_point4 = final_point4 + point4
+            if final_point4 >= 100:
+                final_point = final_point4
+                print(player4_name, " is the winner. Congratulations!")
+
 else:
     print("Please play with minimum 2 and maximum 4 players. Thanks.")
